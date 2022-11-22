220a3b872c7e9241901a685bd2c77fb83e51ee1a
Nicholas <n.goguen-compagnoni@sms.ed.ac.uk>
2022-11-22 09:31:48 -0500

    Fasta read to be implemented

--- gene_search.py
+++ gene_search.py
@@ -1,4 +1,5 @@
 from Bio import Blast
+from Bio import SeqIO, SeqRecord
 import datetime
 EMAIL = "s2013017@ed.ac.uk"
 
@@ -7,34 +8,41 @@ EMAIL = "s2013017@ed.ac.uk"
 
 def search_control(filename):
     #TODO: Implement search control.
-    gene, species, database, algorithm = read_input(filename)
+
+    genefile, species, database, algorithm = read_input(filename)
+
     score = None
     evalue = None
-    return [gene, species, database, algorithm, score, evalue]
+    return [genefile, species, database, algorithm, score, evalue]
 
 #_________________________________________________________
 # File Boilerplate
 
 def read_input(filename):
-    # Since the file is standardized to be in the order gene, species, database, algorithm, output will be in this order
+    # Since the file is standardized to be in the order genefile, species, database, algorithm, output will be in this order
     # and manipulated immediately in search_control which assumes this order of inputs.
+    # TODO: Make more generalizable
     input = []
     file = open(filename, "r")
     for line in file:
-        input.append(line[line.find(": ")+2:]) #+2 excludes the identifier chars.
+        input.append(line[line.find(": ")+2:-1]) #+2 excludes the identifier chars, -1 excludes \n.
     file.close()
     return input
 
-def write_output(filename, gene, species, database, algorithm, score, evalue):
+def write_output(filename, genefile, species, database, algorithm, score, evalue):
     file = open(filename, "w")
 
     date = datetime.datetime.now()
 
-    file.write("Date created: " + str(date) + "\nGene of interest: " + gene + "\nSpecies specified: " + species + 
+    file.write("Date created: " + str(date) + "\nGene of interest: " + genefile + "\nSpecies specified: " + species + 
     "\nDatabase searched: " + database + "\nAlgorithm used: " + algorithm + "\nMax bit score: " + str(score) +
     "\nE-value: " + str(evalue))
 
     file.close()
+
+def read_fasta_sequence():
+    #TODO: Implement.
+    return
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -28,24 +28,36 @@ class TestReadInput(unittest.TestCase):
         f = open("test-search-input.txt", "w")
         f.write("Gene of interest: [genefile]\nSpecies: [species]\nDatabase: [database]\nAlgorithm: [algorithm]\n")
         f.close()
-        [gene, species, database, algorithm] = gene_search.read_input("test-search-input.txt")
+        [genefile, species, database, algorithm] = gene_search.read_input("test-search-input.txt")
 
         os.remove("test-search-input.txt")
 
-        self.assertEqual(gene, "[genefile]\n", f"given gene does not match expected output")
-        self.assertEqual(species, "[species]\n", f"given species does not match expected output")
-        self.assertEqual(database, "[database]\n", f"given database does not match expected output")
-        self.assertEqual(algorithm, "[algorithm]\n", f"given algorithm does not match expected output")
+        self.assertEqual(genefile, "[genefile]", f"given gene does not match expected output")
+        self.assertEqual(species, "[species]", f"given species does not match expected output")
+        self.assertEqual(database, "[database]", f"given database does not match expected output")
+        self.assertEqual(algorithm, "[algorithm]", f"given algorithm does not match expected output")
 
 
 class TestWriteInput(unittest.TestCase):
     def test_runs(self):
-        gene_search.write_output("test-search-output.txt", "[gene]", "[species]", "[database]", "[algorithm]", "[score]", "[evalue]")
+        gene_search.write_output("test-search-output.txt", "[genefile]", "[species]", "[database]", "[algorithm]", "[score]", "[evalue]")
         os.remove("test-search-output.txt")
+        # ad hoc views ascertain the method runs.
     assert True
 
     def test_unexpected_input(self):
         assert True
+
+class TestReadFastaSequence(unittest.TestCase):
+    def test_runs(self):
+        assert True
+        #TODO: finish writing tests
+        f = open("test-search-input.txt", "w")
+        f.write("Gene of interest: /home/nicholas/Downloads/sequence.fasta\nSpecies: [species]\nDatabase: [database]\nAlgorithm: [algorithm]\n")
+        f.close()
+        gene_search.search_control("test-search-input.txt")
+        os.remove("test-search-input.txt")
+        
 
 if __name__ == '__main__':
     unittest.main()
